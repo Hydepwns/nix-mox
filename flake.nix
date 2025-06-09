4695e6ec336eab1894fd2b641378e8cd1cf8beac
DROO <andrewtehsailor@gmail.com>
2025-06-09 05:39:12 +0200

    fix: remove unsupported version attribute from flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
   # - packages.<system>.zfs-snapshot: ZFS snapshot/prune script as a Nix package
 
   description = "Proxmox + NixOS + Windows gaming automation flake";
-  version = "0.1.0";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
