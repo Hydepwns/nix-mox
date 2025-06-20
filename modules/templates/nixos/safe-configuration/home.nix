d7aae8052cd6e6d8bd0b6ba60b7537a4500de8c9
hydepwns <andretehsailor@gmail.com>
2025-06-20 13:23:21 +0200

    feat: add all dev shell aliases to safe configuration home.nix
    - Add aliases for all available nix-mox dev shells
    - Include proper escaping for flake references
    - Add nixos-update command alias for nixos-flake-update
    - Support platform-specific shells (gaming, zfs, macos)
    - Maintain backward compatibility with existing aliases

--- modules/templates/nixos/safe-configuration/home.nix
+++ modules/templates/nixos/safe-configuration/home.nix
@@ -22,9 +22,18 @@
       ngc = "nix-collect-garbage -d";
 
       # Quick access to nix-mox dev shells
-      dev-default = "nix develop ${inputs.nix-mox}#default";
-      dev-gaming = "nix develop ${inputs.nix-mox}#gaming";
-      dev-test = "nix develop ${inputs.nix-mox}#testing";
+      dev-default = "nix develop \${inputs.nix-mox}#default";
+      dev-development = "nix develop \${inputs.nix-mox}#development";
+      dev-testing = "nix develop \${inputs.nix-mox}#testing";
+      dev-services = "nix develop \${inputs.nix-mox}#services";
+      dev-monitoring = "nix develop \${inputs.nix-mox}#monitoring";
+      dev-gaming = "nix develop \${inputs.nix-mox}#gaming";
+      dev-zfs = "nix develop \${inputs.nix-mox}#zfs";
+      dev-macos = "nix develop \${inputs.nix-mox}#macos";
+      dev-storage = "nix develop \${inputs.nix-mox}#storage";
+
+      # nix-mox package commands
+      nixos-update = "nixos-flake-update";
     };
 
     initExtra = ""
